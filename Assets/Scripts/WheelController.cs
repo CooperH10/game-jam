8699f753e14849f391f460b3b99ae5e89ec8f3cc
Aiden Gage <aidenfgage@gmail.com>
2024-02-05 14:16:32 +0100

    spinning arrow records direction on space press

--- Assets/Scripts/WheelController.cs
+++ Assets/Scripts/WheelController.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class WheelController : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+    public Vector2 gravDirection;
+    public float arrowSpeed = 45f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RotateAroundObejct();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gravDirection = transform.up;
+            Debug.Log(gravDirection);
+        }
     }
 
     public Transform objectToRotateAround;
-    void RorateAroundObejct()
+    void RotateAroundObejct()
     {
-        transform.RotateAround(objectToRotateAround.position, Vector3.up, 90);
+        transform.RotateAround(transform.position, Vector3.forward, arrowSpeed * Time.deltaTime);
     }
 }
